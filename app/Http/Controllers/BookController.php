40ed63ede995c6a66a6da13178e9003bbf00d9a1
Ali Tunç Yüngül <78642038+AliTuncY@users.noreply.github.com>
2025-07-04 22:26:19 +0300

    Refactor book scopes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
     public function index(){
-        $books = Book::get();
+        $books = Book::NotDeleted()->get();
         return view('books.index', ['books' => $books]);
     }
     public function create(){
@@ -26,13 +26,13 @@ class BookController extends Controller
     }
 
     public function edit($id){
-        $book = Book::findOrFail($id);
+        $book = Book::NotDeleted()->findOrFail($id);
         return view('books.edit', ['book' => $book]);
     }
 
 
     public function update(Request $request, $id){
-        $book = Book::findOrFail($id);
+        $book = Book::NotDeleted()->findOrFail($id);
         $book->name = $request->name;
         $book->price = $request->price;
         $book->save();
@@ -41,7 +41,7 @@ class BookController extends Controller
     }
 
     public function delete($id){
-        $book = Book::findOrFail($id)->delete();
+        $book = Book::findOrFail($id)->update(['is_deleted' => 1]);
         return redirect()->back();
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = ['is_deleted'];
+
+    public function scopeNotDeleted($query){
+        return $query->where('is_deleted', 0);
+    }
 }
